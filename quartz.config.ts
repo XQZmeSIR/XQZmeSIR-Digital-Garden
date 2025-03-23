766f3ffe60c4042e08d7388e47372c0eaf9a0864
XQZmeSIR <88614392+XQZmeSIR@users.noreply.github.com>
2025-03-23 15:06:32 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
         header: "Inconsolata",
         // header: "Schibsted Grotesk",
         // body: "Source Sans Pro",
-        body: "Inconsolata",
+        body: "M PLUS Rounded 1c", //"Inconsolata",
         code: "JetBrains Mono",
       },
       colors: {
