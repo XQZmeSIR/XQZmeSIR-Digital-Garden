c1ebf1f58d555acda0ac57583c481c2f04810b60
XQZmeSIR <88614392+XQZmeSIR@users.noreply.github.com>
2025-04-18 08:07:36 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
         header: "Rubik",
         // header: "Schibsted Grotesk",
         // body: "Source Sans Pro", M PLUS Rounded 1c, Exo 2, Libre Franklin, Vollkorn, JetBrains Mono        
-        body: "JetBrains Mono", //"PT Sans", //"Inconsolata",
+        body: "Nunito", //"PT Sans", //"Inconsolata",
         code: "JetBrains Mono",
       },
       colors: {
