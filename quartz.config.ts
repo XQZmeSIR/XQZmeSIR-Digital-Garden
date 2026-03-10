369bdd76a96dae30dd7e371598dfab4874dac425
XQZmeSIR <88614392+XQZmeSIR@users.noreply.github.com>
2026-03-10 11:03:53 +0300

    font change and explorer font decrease

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,10 +20,10 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Space Grotesk",
+        header: "Russo One",
         // header: "Schibsted Grotesk",
         // body: "Source Sans Pro", M PLUS Rounded 1c, Exo 2, Libre Franklin, Vollkorn, JetBrains Mono, Gidole, Rubik
-        body: "Space Grotesk", //"PT Sans", //"Inconsolata",
+        body: "Space Grotesk", "Unbounded", //"PT Sans", //"Inconsolata",
         code: "Space Mono",
       },
       colors: {
