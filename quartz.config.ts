51f100c61b49e95193e86cc754b0cebf0074093e
XQZmeSIR <88614392+XQZmeSIR@users.noreply.github.com>
2026-03-12 01:37:59 +0300

    Changed created modified property for displaying when the change happened in a note.

--- quartz.config.ts
+++ quartz.config.ts
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git"],
+        priority: ["git", "frontmatter"],
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
